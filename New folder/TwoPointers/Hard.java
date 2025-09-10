56c09dd68e01e1f96e46c080638ac9e743dc4554
laloeltigueeere <laloeltigueeere@gmail.com>
2025-09-09 18:56:03 -0600

    Tuesday

--- New folder/TwoPointers/Hard.java
+++
@@ -1,4 +0,0 @@
-// Given an array height[] representing the elevation map, calculate how much water can be trapped between the bars after raining.
-// int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-
-
---
+++ aerobics/TwoPointers/Palindromo.java
@@ -0,0 +1,19 @@
+package aerobics.TwoPointers;
+class Palindromo {
+    public static boolean isPalindromo(String word) {
+        String normalizedWord = word.replace(" ", "").toUpperCase();
+        int left = 0, right = normalizedWord.length() - 1;
+        while (left < right) {
+            if (normalizedWord.charAt(left) != normalizedWord.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String word = "Anita laba la tina";
+        System.out.println(isPalindromo(word) ? "Si es " : "No es");
+    }
+}
